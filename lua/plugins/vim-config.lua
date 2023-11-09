532b076868455da90658ee29e84110813b534474
Madhur Toppo <madhur.toppo@gmail.com>
2023-11-10 01:37:04 +0530

    Add vim configuration

---
+++ lua/plugins/vim-config.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      vim.cmd("set nocursorline"),
+      vim.cmd("hi LineNrAbove guifg=SeaGreen3"),
+      vim.cmd("hi LineNrBelow guifg=HotPink1"),
+    },
+  },
+}
