619a9c2b77d34f2071719b69ea898eab43b5f7be
Madhur Toppo <madhur.toppo@gmail.com>
2023-11-10 03:08:44 +0530

    Add LineNr color

--- lua/plugins/vim-config.lua
+++ lua/plugins/vim-config.lua
@@ -3,6 +3,7 @@ return {
     "LazyVim/LazyVim",
     opts = {
       vim.cmd("set nocursorline"),
+      vim.cmd("hi LineNr guibg=tan2 guifg=tan1"),
       vim.cmd("hi LineNrAbove guifg=SeaGreen3"),
       vim.cmd("hi LineNrBelow guifg=HotPink1"),
     },
