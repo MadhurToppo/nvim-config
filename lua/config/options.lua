6599db26197c0adb486fc72d73dc95b87d163c28
Madhur Toppo <madhur.toppo@gmail.com>
2023-11-16 01:10:56 +0530

    Remove cursor line number color change

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.cmd("set nocursorline")
-vim.cmd("hi LineNr guibg=tan2 guifg=tan1")
 vim.cmd("hi LineNrAbove guifg=SeaGreen3")
 vim.cmd("hi LineNrBelow guifg=HotPink1")
