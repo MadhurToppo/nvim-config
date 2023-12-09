5a6f6b4143422376ea8e001003efccb2782904b4
Madhur Toppo <madhur.toppo@gmail.com>
2023-12-09 21:02:10 +0530

    Remove markdown-preview auto-start and auto-close

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,3 @@
 -- Add any additional options here
 vim.cmd("hi LineNrAbove guifg=SeaGreen3")
 vim.cmd("hi LineNrBelow guifg=HotPink1")
-vim.cmd("let g:mkdp_auto_start = 1")
-vim.cmd("let g:mkdp_auto_close = 1")
