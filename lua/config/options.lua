95a149c458a7a6bdfdbf57fc056452f769738678
Madhur Toppo <madhur.toppo@gmail.com>
2023-12-09 23:53:54 +0530

    Add blinking cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 vim.cmd("hi LineNrAbove guifg=SeaGreen3")
 vim.cmd("hi LineNrBelow guifg=HotPink1")
+vim.opt.guicursor = {
+  "n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100",
+  "r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100",
+}
