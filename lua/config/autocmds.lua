dc1ffa3edccbd734cab6f7a19e63ff0a06da253f
Madhur Toppo <madhur.toppo@gmail.com>
2024-01-18 00:13:20 +0530

    Disable neotre on startup

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,3 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-if vim.fn.argc(-1) == 0 then
-  vim.cmd("Neotree")
-  vim.cmd("set norelativenumber")
-  vim.cmd("set nonumber")
-end
