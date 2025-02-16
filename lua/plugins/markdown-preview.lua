1916fcd89ffc60618ee661993ed64a9d0909892b
Madhur Toppo <madhur.toppo@gmail.com>
2025-02-16 03:07:34 +0100

    Add markdown plugins

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -5,8 +5,16 @@ return {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview toggle" },
     },
     ft = "markdown",
-    build = function()
+    build = function(hl)
       vim.fn["mkdp#util#install"]()
+      -- Remove heading highlights
+      hl.Title = { fg = "NONE", bg = "NONE" }
+      hl.markdownH1 = { fg = "NONE", bg = "NONE" }
+      hl.markdownH2 = { fg = "NONE", bg = "NONE" }
+      hl.markdownH3 = { fg = "NONE", bg = "NONE" }
+      hl.markdownH4 = { fg = "NONE", bg = "NONE" }
+      hl.markdownH5 = { fg = "NONE", bg = "NONE" }
+      hl.markdownH6 = { fg = "NONE", bg = "NONE" }
     end,
   },
 }
--- lua/plugins/vim-markdown.lua
+++ lua/plugins/vim-markdown.lua
@@ -1,6 +1,7 @@
 return {
   {
     "plasticboy/vim-markdown",
+    enabled = false,
     branch = "master",
     require = { "godlygeek/tabular" },
   },
