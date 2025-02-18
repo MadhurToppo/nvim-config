3a17b16760789276242a54f615be57bfed3395aa
Madhur Toppo <madhur.toppo@gmail.com>
2025-02-18 01:16:15 +0100

    Update plugins

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -7,14 +7,6 @@ return {
     ft = "markdown",
     build = function(hl)
       vim.fn["mkdp#util#install"]()
-      -- Remove heading highlights
-      hl.Title = { fg = "NONE", bg = "NONE" }
-      hl.markdownH1 = { fg = "NONE", bg = "NONE" }
-      hl.markdownH2 = { fg = "NONE", bg = "NONE" }
-      hl.markdownH3 = { fg = "NONE", bg = "NONE" }
-      hl.markdownH4 = { fg = "NONE", bg = "NONE" }
-      hl.markdownH5 = { fg = "NONE", bg = "NONE" }
-      hl.markdownH6 = { fg = "NONE", bg = "NONE" }
     end,
   },
 }
