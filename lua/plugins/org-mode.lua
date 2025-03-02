1a143a4c496f41c61de9f563d6260c0593c03cf2
Madhur Toppo <madhur.toppo@gmail.com>
2025-03-02 02:20:10 +0100

    Add keympas for todos status change

--- lua/plugins/org-mode.lua
+++ lua/plugins/org-mode.lua
@@ -2,11 +2,10 @@ return {
   "nvim-orgmode/orgmode",
   event = "VeryLazy",
   config = function()
-    -- Setup orgmode
     require("orgmode").setup({
       org_agenda_files = "~/workspace/journal/*.org",
       org_default_notes_file = "~/workspace/journal/refile.org",
-      org_todo_keywords = { "TODO", "SCHEDULED", "ACTIVE", "PAUSED", "|", "DONE", "DECLINED" },
+      org_todo_keywords = { "TODO(t)", "SCHEDULED(s)", "ACTIVE(a)", "PAUSED(p)", "DECLINED(x)", "|", "DONE(d)" },
       org_todo_keyword_faces = {
         TODO = ":foreground red",
         SCHEDULED = ":foreground yellow",
