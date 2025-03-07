06a13234f275a98eeec68e94b3c7293adf64fda2
Madhur Toppo <madhur.toppo@gmail.com>
2025-03-07 02:31:49 +0100

    Update org-mode and lazyvim plugins

--- lua/plugins/org-mode.lua
+++ lua/plugins/org-mode.lua
@@ -1,9 +1,14 @@
 return {
   "nvim-orgmode/orgmode",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-orgmode/telescope-orgmode.nvim",
+    "nvim-orgmode/org-bullets.nvim",
+  },
   event = "VeryLazy",
   config = function()
     require("orgmode").setup({
-      org_agenda_files = "~/workspace/journal/*.org",
+      org_agenda_files = "~/workspace/journal/**/*",
       org_default_notes_file = "~/workspace/journal/refile.org",
       org_todo_keywords = { "TODO(t)", "SCHEDULED(s)", "ACTIVE(a)", "PAUSED(p)", "DECLINED(x)", "|", "DONE(d)" },
       org_todo_keyword_faces = {
@@ -11,8 +16,8 @@ return {
         SCHEDULED = ":foreground yellow",
         ACTIVE = ":foreground magenta",
         PAUSED = ":foreground violet",
-        DONE = ":foreground green",
         DECLINED = ":foreground turquoise",
+        DONE = ":foreground green",
       },
       mappings = {
         org = {
@@ -20,5 +25,8 @@ return {
         },
       },
     })
+    require("org-bullets").setup()
+    require("telescope").setup({})
+    require("telescope").load_extension("orgmode")
   end,
 }
