df62cbe11a39b1cda11b4ada575d7a14ea872f0e
Madhur Toppo <madhur.toppo@gmail.com>
2025-03-14 00:18:21 +0100

    Change order of todo states

--- lua/plugins/org-mode.lua
+++ lua/plugins/org-mode.lua
@@ -10,14 +10,14 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/workspace/journal/**/*",
       org_default_notes_file = "~/workspace/journal/refile.org",
-      org_todo_keywords = { "TODO(t)", "SCHEDULED(s)", "ACTIVE(a)", "PAUSED(p)", "DECLINED(x)", "|", "DONE(d)" },
+      org_todo_keywords = { "TODO(t)", "SCHEDULED(s)", "ACTIVE(a)", "PAUSED(p)", "|", "DONE(d)", "DECLINED(x)" },
       org_todo_keyword_faces = {
         TODO = ":foreground red",
         SCHEDULED = ":foreground yellow",
         ACTIVE = ":foreground magenta",
         PAUSED = ":foreground violet",
-        DECLINED = ":foreground turquoise",
         DONE = ":foreground green",
+        DECLINED = ":foreground turquoise",
       },
       mappings = {
         org = {
